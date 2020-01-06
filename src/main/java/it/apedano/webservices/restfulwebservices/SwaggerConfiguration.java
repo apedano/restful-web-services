3b8f3e9d967cbae12c2a0c7f444d41b30a888c8d
Alessandro Pedano <alessandro.pedano@transsmart.com>
2020-01-06 11:23:10 +0100

    Mmonitoring with actuators

---
+++ src/main/java/it/apedano/webservices/restfulwebservices/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+/*
+ * COPYRIGHT NOTICE
+ * © 2020  Transsmart Holding B.V.
+ * All Rights Reserved.
+ * All information contained herein is, and remains the
+ * property of Transsmart Holding B.V. and its suppliers,
+ * if any.
+ * The intellectual and technical concepts contained herein
+ * are proprietary to Transsmart Holding B.V. and its
+ * suppliers and may be covered by European and Foreign
+ * Patents, patents in process, and are protected by trade
+ * secret or copyright law.
+ * Dissemination of this information or reproduction of this
+ * material is strictly forbidden unless prior written
+ * permission is obtained from Transsmart Holding B.V.
+ */
+package it.apedano.webservices.restfulwebservices;
+
+/**
+ *
+ * @author Alessandro Pedano <alessandro.pedano@transsmart.com>
+ */
+//@Configuration //to be used as SPring configuration
+//@EnableSwagger2
+public class SwaggerConfiguration {
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2);
+//    }
+}
